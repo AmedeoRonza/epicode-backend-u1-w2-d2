a797390172364a47a9fae6ce72e98e41ae885ea5
AmedeoRonza <148001313+AmedeoRonza@users.noreply.github.com>
2024-02-06 15:39:13 +0100

    Add files via upload

---
+++ Esercizio2.sql
@@ -0,0 +1,8 @@
+--select * from Products
+--select * from Products where UnitsInStock>=40
+--select * from Employees where City = 'London' 
+--select * from Orders order by Freight desc
+--select * from Orders where Freight >= 90 and Freight < 200
+--select * from Products where CategoryID = 1
+--select * from [Order Details] where Discount<>0
+--select * from Orders where CustomerID = 'BOTTM' and Freight > 50
